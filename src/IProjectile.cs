059a7edda543c24f0f1ad1bab523779dc32851df
Theo Wiik <theo.le.wiik@gmail.com>
2019-10-30 17:34:29 +0100

    create mock of iprojectile

---
+++ src/IProjectile.cs
@@ -0,0 +1,20 @@
+using Godot
+
+interface IProjectile
+{
+  /// <summary>
+  /// Destroys the projectile.
+  /// </summary>
+  void destroy();
+
+  /// <summary>
+  /// Moves the projectile with 
+  /// </summary>
+  void move();
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <param name="direction"></param>
+  void append(Vector2 direction);
+}
