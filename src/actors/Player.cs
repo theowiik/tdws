c24c6578b323deb3933fc13c35225108b05da26c
Theo Wiik <theo.le.wiik@gmail.com>
2019-10-30 10:23:39 +0100

    add player

---
+++ src/actors/Player.cs
@@ -0,0 +1,16 @@
+using System;
+using Godot;
+
+/// <summary>
+/// The Player charachter.
+/// </summary>
+public class Player : KinematicBody2D
+{
+  public override void _Ready()
+  {
+  }
+
+  public override void _Process(float delta)
+  {
+  }
+}
