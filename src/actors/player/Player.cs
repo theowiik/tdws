1e4d3112a4b2345ec098b3df0aa905cec1e0c4b9
Theo Wiik <theo.le.wiik@gmail.com>
2019-10-30 17:18:09 +0100

    add ability for player to shoot projectiles

--- src/actors/player/Player.cs
+++ src/actors/player/Player.cs
@@ -4,21 +4,58 @@ using Godot;
 /// <summary>
 /// The Player charachter.
 /// </summary>
-public class Player : KinematicBody2D
+public sealed class Player : KinematicBody2D
 {
   private Vector2 velocity;
   private int movementSpeed;
+  private PackedScene projectile;
 
   public override void _Ready()
   {
     velocity = new Vector2();
     movementSpeed = 300;
+    projectile = GD.Load("res://src/objects/projectile/Projectile.tscn") as PackedScene;
   }
 
   public override void _Process(float delta)
   {
     velocity = getMovementInputVector() * movementSpeed;
     velocity = MoveAndSlide(velocity);
+
+    if (Input.IsActionPressed("shoot"))
+    {
+      shoot();
+    }
+  }
+
+  /// <summary>
+  /// Creates a new projectile with the direction pointing towards the mouse.
+  /// </summary>
+  private void shoot()
+  {
+    Projectile proj = projectile.Instance() as Projectile;
+    GetParent().AddChild(proj);
+    proj.SetPosition(Transform.origin);
+    proj.setDirection(playerToMouseVec());
+  }
+
+  /// <summary>
+  /// Returns the direction vector from the player to the mouse. Returns a null vector if
+  /// the global mouse position is null.
+  /// </summary>
+  ///
+  /// <returns>
+  /// The direction vector from the player to the mouse.
+  /// </returns>
+  private Vector2 playerToMouseVec()
+  {
+    Vector2 mousePos = GetGlobalMousePosition();
+    if (mousePos == null) return new Vector2();
+
+    return new Vector2(
+      mousePos.x - Transform.origin.x,
+      mousePos.y - Transform.origin.y
+    ).Normalized();
   }
 
   /// <summary>
