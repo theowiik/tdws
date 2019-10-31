83c2317573a6257f47c20c6f51a4c0cfe3473f41
theowiik <theo.le.wiik@gmail.com>
2019-10-31 09:41:28 +0100

    scroll holster

--- src/actors/player/Holster.cs
+++ src/actors/player/Holster.cs
@@ -10,17 +10,55 @@ public class Holster : Node
 {
   private List<IProjectileShooter> _projectileShooters;
   private int _inventoryIndex;
+  private int _maxInventorySize;
 
   public override void _Ready()
   {
     _inventoryIndex = 0;
+    _maxInventorySize = 5;
     _projectileShooters = new List<IProjectileShooter>(10);
   }
 
   public override void _Process(float delta)
   {
-    if (Input.IsActionPressed("weapon_next")) GD.Print("do something ...");
-    if (Input.IsActionPressed("weapon_previous")) GD.Print("do something else ...");
+    if (Input.IsActionJustReleased("weapon_next")) NextWeapon();
+    if (Input.IsActionJustReleased("weapon_previous")) PreviousWeapon();
+
+    GD.Print(_inventoryIndex);
+  }
+
+  /// <summary>
+  /// Increases the _inventoryIndex value.
+  /// Sets the _inventoryIndex to 0 if it is greater or equal to the max inventory size.
+  /// </summary>
+  private void NextWeapon()
+  {
+    _inventoryIndex++;
+    setClosestLegalInventoryIndex();
+  }
+
+  /// <summary>
+  /// Decreases the _inventoryIndex value.
+  /// Sets the _inventoryIndex to the max inventory size if it is smaller than 0.
+  /// </summary>
+  private void PreviousWeapon()
+  {
+    _inventoryIndex--;
+    setClosestLegalInventoryIndex();
+  }
+
+  /// <summary>
+  /// Sets the _inventoryIndex to the closest legal value.
+  /// 
+  /// ... why doesn't C# have regular modulo?
+  /// </summary>
+  private void setClosestLegalInventoryIndex()
+  {
+    if (_inventoryIndex < 0)
+      _inventoryIndex = (_maxInventorySize - 1);
+
+    if (_inventoryIndex >= _maxInventorySize)
+      _inventoryIndex = 0;
   }
 
   /// <summary>
