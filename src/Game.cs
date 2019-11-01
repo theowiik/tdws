02a7d304bde30b8c13279cbce2f0650f0c2c3b52
theowiik <theo.le.wiik@gmail.com>
2019-11-01 17:04:58 +0100

    refactor Game.cs

--- src/Game.cs
+++ src/Game.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 
 /// <summary>
 /// The Game class is the main class.
@@ -8,7 +7,7 @@ public class Game : Node2D
 {
   public override void _Input(InputEvent @event)
   {
-    // Close if ui_cancel is pressed.
-    if (@event.IsActionPressed("ui_cancel")) GetTree().Quit();
+    if (@event.IsActionPressed("ui_cancel"))
+      GetTree().Quit();
   }
 }
