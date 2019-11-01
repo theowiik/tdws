d5df741daf69f5d187e02f53a363978ce65aa9ad
theowiik <theo.le.wiik@gmail.com>
2019-11-01 17:10:35 +0100

    basic functionality for Walk state

--- src/actors/player/states/motion/on_ground/OnGround.cs
+++ src/actors/player/states/motion/on_ground/OnGround.cs
@@ -1,10 +1,25 @@
 using Godot;
-using System;
 
 /// <summary>
 /// The OnGround class is the base class that all on ground states extends.
 /// </summary>
-public class OnGround : Node
+public class OnGround : Motion
 {
+  // TODO: Refactor to private double and add a setter?
+  public double Speed { get; set; }
+  private Vector2 _velocity;
 
+  public override void _Ready()
+  {
+    Speed = 0.0;
+    _velocity = new Vector2();
+  }
+
+  public void HandleInput(InputEvent @event)
+  {
+    if (@event.IsActionPressed("jump"))
+    {
+      GD.Print("Jump!");
+    }
+  }
 }
--- src/actors/player/states/motion/on_ground/Walk.cs
+++ src/actors/player/states/motion/on_ground/Walk.cs
@@ -1,4 +1,13 @@
+/// <summary>
+/// The Walk class is used for handling walking movement.
+/// </summary>
 public sealed class Walk : OnGround
 {
-  
+  private const int _MaxWalkSpeed = 3;
+  private const int _MaxRunSpeed = 3;
+
+  public void Enter()
+  {
+    Speed = 0.0;
+  }
 }
