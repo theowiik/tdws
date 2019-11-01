e76100d24f1a2ba1312b5f9f297e38ab012fe535
theowiik <theo.le.wiik@gmail.com>
2019-11-01 15:05:11 +0100

    add IState interface

---
+++ src/utils/state/IState.cs
@@ -0,0 +1,27 @@
+using Godot;
+
+/// <summary>
+/// The IState interface is a interface for all states.
+/// </summary>
+public interface IState
+{
+  /// <summary>
+  /// Initializes the state.
+  /// </summary>
+  void Enter();
+
+  /// <summary>
+  /// Disables the state.
+  /// </summary>
+  void Exit();
+
+  /// <summary>
+  /// The entry point for when new events occur.
+  /// </summary>
+  void HandleInput(InputEvent @event);
+
+  /// <summary>
+  /// Updates the state.
+  /// </summary>
+  void Update();
+}
