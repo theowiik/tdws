b936d7105c02544859f9248440d4a71ec2c75552
theowiik <theo.le.wiik@gmail.com>
2019-11-02 11:42:34 +0100

    implement walk state

--- src/actors/player/states/motion/on_ground/Walk.cs
+++ src/actors/player/states/motion/on_ground/Walk.cs
@@ -1,13 +1,26 @@
+using Godot;
+
 /// <summary>
 /// The Walk class is used for handling walking movement.
 /// </summary>
 public sealed class Walk : OnGround
 {
-  private const int _MaxWalkSpeed = 3;
-  private const int _MaxRunSpeed = 3;
+  private const int _MaxWalkSpeed = 300;
 
-  public void Enter()
+  public override void Enter()
   {
     Speed = 0.0;
+  }
+
+  public override void Exit()
+  {
+    // do something
+  }
+
+  public override void Update()
+  {
+    Vector2 inputDirection = GetMovementInputVector();
+    _velocity = inputDirection * _MaxWalkSpeed;
+    ((PlayerController)Owner).Move(_velocity);
   }
 }
