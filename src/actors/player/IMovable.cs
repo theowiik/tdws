a2ca110e9b4dac662ac6b89a93987f27cc28a307
theowiik <theo.le.wiik@gmail.com>
2019-11-03 12:31:44 +0100

    add imovable interface

---
+++ src/actors/player/IMovable.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+/// <summary>
+/// The IMovable interface represents objects that can move.
+/// </summary>
+public interface IMovable
+{
+  /// <summary>
+  /// Moves the movable object with the given velocity.
+  /// </summary>
+  ///
+  /// <param name="velocity">
+  /// The velocity to move the movable object with.
+  /// </param>
+  void Move(Vector2 velocity);
+}
--- src/actors/player/PlayerController.cs
+++ src/actors/player/PlayerController.cs
@@ -3,7 +3,7 @@ using Godot;
 /// <summary>
 /// The Player charachter.
 /// </summary>
-public sealed class PlayerController : AbstractActor
+public sealed class PlayerController : AbstractActor, IMovable
 {
   private Holster _holster;
   private AnimationPlayer _animationPlayer;
@@ -25,34 +25,8 @@ public sealed class PlayerController : AbstractActor
 
   public override void _PhysicsProcess(float delta) { }
 
-  /// <summary>
-  /// Removes all child nodes from the _projectileShooterHolder node.
-  /// </summary>
-  private void UnequipProjectileShooters()
-  {
-    foreach (Node child in _projectileShooterHolder.GetChildren())
-      _projectileShooterHolder.RemoveChild(child);
-  }
-
-  /// <summary>
-  /// Moves the player with the given velocity.
-  /// </summary>
-  ///
-  /// <param name="velocity">
-  /// The velocity to be added to the player.
-  /// </param>
-  public void Move(Vector2 velocity)
+  void IMovable.Move(Vector2 velocity)
   {
     MoveAndSlide(velocity);
-  }
-
-  /// <summary>
-  /// Unequips all projectile shooters and equips the projectile shooter that is currently held.
-  /// </summary>
-  private void EquipHoldingProjectileShooter()
-  {
-    UnequipProjectileShooters();
-    var holding = _holster.GetHolding() as Node;
-    _projectileShooterHolder.AddChild(holding);
   }
 }
