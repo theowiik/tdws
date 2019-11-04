debd82efd47a330d749f2e1aecd2a5a2c6f0db4e
Theo Wiik <theo.le.wiik@gmail.com>
2019-11-04 20:14:10 +0100

    add crate.cs

---
+++ src/objects/crate/Crate.cs
@@ -0,0 +1,25 @@
+﻿using Godot;
+using tdws.projectile_shooters;
+
+namespace tdws.objects.crate
+{
+  /// <summary>
+  ///   The Crate class holds a projectile shooter.
+  /// </summary>
+  public class Crate : Area2D
+  {
+    public IProjectileShooter ProjectileShooter { get; private set; }
+
+    public override void _Ready()
+    {
+      ProjectileShooter = ProjectileShooterFactory.CreateShotgun();
+    }
+
+    /// <summary>
+    /// Picks up the projectile shooter.
+    /// </summary>
+    public void PickUp()
+    {
+    }
+  }
+}
