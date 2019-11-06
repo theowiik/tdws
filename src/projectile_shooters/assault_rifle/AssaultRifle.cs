beee4d2f9ea1d3a9a8597f32896a686ed4cc8ff8
Theo Wiik <theo.le.wiik@gmail.com>
2019-11-06 12:11:34 +0100

    create assault rifle

---
+++ src/projectile_shooters/assault_rifle/AssaultRifle.cs
@@ -0,0 +1,14 @@
+using tdws.projectile_shooters.projectile_shooter;
+
+namespace tdws.projectile_shooters.assault_rifle
+{
+  /// <summary>
+  ///   A assault rifle.
+  /// </summary>
+  public class AssaultRifle : ProjectileShooter
+  {
+    protected override void OverrideProperties()
+    {
+    }
+  }
+}
