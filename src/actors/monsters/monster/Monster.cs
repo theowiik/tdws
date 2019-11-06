26eb6c22ba8a2257e39f97321e22d63740440c4b
theowiik <theo.le.wiik@gmail.com>
2019-11-06 19:19:22 +0100

    add abstract monster

---
+++ src/actors/monsters/monster/Monster.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+namespace tdws.actors.monsters.monster
+{
+  /// <summary>
+  ///   A abstract monster.
+  /// </summary>
+  public sealed class Monster : KinematicBody2D
+  {
+    public override void _Ready()
+    {
+    }
+  }
+}
