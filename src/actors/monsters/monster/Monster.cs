a9b86cbad0a24838cfbe27a2bcd62066d86564a7
theowiik <theo.le.wiik@gmail.com>
2019-11-06 19:24:32 +0100

    add skeleton

--- src/actors/monsters/monster/Monster.cs
+++ src/actors/monsters/monster/Monster.cs
@@ -1,14 +1,18 @@
 using Godot;
+using tdws.actors.stats;
 
 namespace tdws.actors.monsters.monster
 {
   /// <summary>
   ///   A abstract monster.
   /// </summary>
-  public sealed class Monster : KinematicBody2D
+  public abstract class Monster : KinematicBody2D
   {
+    protected Stats stats;
+
     public override void _Ready()
     {
+      stats = GetNode("Stats") as Stats;
     }
   }
 }
---
+++ src/actors/monsters/skeleton/Skeleton.cs
@@ -0,0 +1,8 @@
+using tdws.actors.monsters.monster;
+
+namespace tdws.actors.monsters.skeleton
+{
+  public class Skeleton : Monster
+  {
+  }
+}
