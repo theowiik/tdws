5f0b93b72d063722bf7bb3c9bdd0144ef03a89d7
theowiik <theo.le.wiik@gmail.com>
2019-11-07 21:25:06 +0100

    add i damage source

---
+++ src/objects/IDamageSource.cs
@@ -0,0 +1,16 @@
+﻿namespace tdws.objects
+{
+  /// <summary>
+  ///   Represents something that can deal damage.
+  /// </summary>
+  public interface IDamageSource
+  {
+    /// <summary>
+    ///   Returns the amount of damage that the object deals.
+    /// </summary>
+    /// <returns>
+    ///   The amount of damage that the object deals.
+    /// </returns>
+    int GetDamage();
+  }
+}
