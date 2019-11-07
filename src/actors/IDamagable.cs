d205b45308628ab6b5167ae7bb1f52d6da0c307e
theowiik <theo.le.wiik@gmail.com>
2019-11-07 21:33:48 +0100

    add damagable interface

---
+++ src/actors/IDamagable.cs
@@ -0,0 +1,18 @@
+﻿using tdws.objects;
+
+namespace tdws.actors
+{
+  /// <summary>
+  ///   The IDamagable interface represents something that can take damage.
+  /// </summary>
+  public interface IDamagable
+  {
+    /// <summary>
+    ///   Take damage from a damage source.
+    /// </summary>
+    /// <param name="damageSource">
+    ///   The source of the damage.
+    /// </param>
+    void TakeDamage(IDamageSource damageSource);
+  }
+}
--- src/actors/monsters/monster/Monster.cs
+++ src/actors/monsters/monster/Monster.cs
@@ -1,14 +1,20 @@
 using Godot;
 using tdws.actors.stats;
+using tdws.objects;
 
 namespace tdws.actors.monsters.monster
 {
   /// <summary>
   ///   A abstract monster.
   /// </summary>
-  public abstract class Monster : KinematicBody2D
+  public abstract class Monster : KinematicBody2D, IDamagable
   {
     protected Stats stats;
+
+    public void TakeDamage(IDamageSource damageSource)
+    {
+      GD.Print("oof! Took " + damageSource.GetDamage() + " damage!");
+    }
 
     public override void _Ready()
     {
