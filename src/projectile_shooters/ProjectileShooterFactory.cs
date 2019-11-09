e76d8270e2a418c3905fd002ad4056d468c0817c
theowiik <theo.le.wiik@gmail.com>
2019-11-09 15:48:34 +0100

    add factory method for wonky gun

--- src/projectile_shooters/ProjectileShooterFactory.cs
+++ src/projectile_shooters/ProjectileShooterFactory.cs
@@ -6,7 +6,7 @@ namespace tdws.projectile_shooters
   /// <summary>
   ///   The ProjectileShooterFactory is used for creating IProjectileShooter implementations.
   /// </summary>
-  public sealed class ProjectileShooterFactory
+  public static class ProjectileShooterFactory
   {
     /// <summary>
     ///   Creates and returns a basic automatic rifle.
@@ -40,6 +40,24 @@ namespace tdws.projectile_shooters
       if (!(GD.Load("res://src/projectile_shooters/shotgun/Shotgun.tscn") is PackedScene
         projectileShooter))
         throw new FileNotFoundException("Could not find Shotgun.tscn");
+
+      return projectileShooter.Instance() as IProjectileShooter;
+    }
+
+    /// <summary>
+    ///   Creates and returns a wonky gun.
+    /// </summary>
+    /// <returns>
+    ///   A wonky gun.
+    /// </returns>
+    /// <exception cref="FileNotFoundException">
+    ///   If the projectile shooter scene is not found.
+    /// </exception>
+    public static IProjectileShooter CreateWonkyGun()
+    {
+      if (!(GD.Load("res://src/projectile_shooters/wonky_gun/WonkyGun.tscn") is PackedScene
+        projectileShooter))
+        throw new FileNotFoundException("Could not find WonkyGun.tscn");
 
       return projectileShooter.Instance() as IProjectileShooter;
     }
