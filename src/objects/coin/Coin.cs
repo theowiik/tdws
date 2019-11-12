f922275b0e332db764f145e46f9fa5469acd5795
theowiik <theo.le.wiik@gmail.com>
2019-11-12 20:25:58 +0100

    add layers and remove coins when body entered

--- src/objects/coin/Coin.cs
+++ src/objects/coin/Coin.cs
@@ -8,5 +8,16 @@ namespace tdws.objects.coin
   public class Coin : Area2D
   {
     private const int Value = 100;
+
+    /// <summary>
+    ///   Is called when a body enters the coin.
+    /// </summary>
+    /// <param name="body">
+    ///   The body that entered the coin.
+    /// </param>
+    private void OnBodyEntered(object body)
+    {
+      QueueFree();
+    }
   }
 }
