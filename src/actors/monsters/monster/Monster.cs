2859603197f10c9491636c8676fcc35eb77672a0
Theo Wiik <theo.le.wiik@gmail.com>
2019-11-12 16:47:27 +0100

    make monsters chase the player

--- src/actors/monsters/monster/Monster.cs
+++ src/actors/monsters/monster/Monster.cs
@@ -7,9 +7,15 @@ namespace tdws.actors.monsters.monster
   /// <summary>
   ///   A abstract monster.
   /// </summary>
-  public abstract class Monster : KinematicBody2D, IDamageable
+  public abstract class Monster : KinematicBody2D, IDamageable, IDamageSource
   {
     private PackedScene _deathEffect;
+
+    /// <summary>
+    ///   The target destination.
+    /// </summary>
+    private KinematicBody2D _target;
+
     protected Stats stats;
 
     public void TakeDamage(IDamageSource damageSource)
@@ -24,18 +30,40 @@ namespace tdws.actors.monsters.monster
     public void Die()
     {
       QueueFree();
-      
+
       if (!(_deathEffect.Instance() is Particles2D particles))
         return;
-      
+
       particles.SetGlobalPosition(GetGlobalPosition());
       GetParent().AddChild(particles);
+    }
+
+    public int GetDamage()
+    {
+      return 10;
+    }
+
+    private void _on_Area2D_body_entered(object body)
+    {
+      GD.Print("ENTER!");
+
+      if (body is KinematicBody2D body2D)
+        _target = body2D;
     }
 
     public override void _Ready()
     {
       stats = GetNode("Stats") as Stats;
       _deathEffect = GD.Load("res://src/particles/DeathEffect.tscn") as PackedScene;
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+      if (_target == null) return;
+
+      GD.Print("MOVE!!!!");
+      var toTarget = GetGlobalPosition().DirectionTo(_target.GetGlobalPosition());
+      MoveAndSlide(toTarget.Normalized() * 100);
     }
   }
 }
