fc62f115dff605c9313a22a80bdb3493d7663d51
theowiik <theo.le.wiik@gmail.com>
2019-11-12 20:09:43 +0100

    create a simple coin

---
+++ src/objects/coin/Coin.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+namespace tdws.objects.coin
+{
+  /// <summary>
+  ///   A coin. TODO: Make it implement something like pickupable.
+  /// </summary>
+  public class Coin : Area2D
+  {
+    private const int Value = 100;
+  }
+}
