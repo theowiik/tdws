c791d0138b5bb5ba0dacc31b322a5edf9c83e0f3
theowiik <theo.le.wiik@gmail.com>
2019-11-17 22:16:51 +0100

    collide on walls

---
+++ src/objects/box/Box.cs
@@ -0,0 +1,15 @@
+using Godot;
+using tdws.actors.player;
+
+namespace tdws.objects.box
+{
+  /// <summary>
+  ///   A pushable box.
+  /// </summary>
+  public class Box : RigidBody2D
+  {
+    public override void _PhysicsProcess(float delta)
+    {
+    }
+  }
+}
