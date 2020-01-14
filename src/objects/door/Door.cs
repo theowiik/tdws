aea689ef1a51db517910ddb84a2a8b640a6a9cfe
Theo Wiik <theo.le.wiik@gmail.com>
2020-01-14 13:07:50 +0100

    display a "enter" text when standing in front of a door

--- src/objects/door/Door.cs
+++ src/objects/door/Door.cs
@@ -7,12 +7,24 @@ namespace tdws.objects.door
     [Signal]
     public delegate void DoorEntered();
 
+    private RichTextLabel _enterText;
+
+    public override void _Ready()
+    {
+      _enterText = GetNode("EnterText") as RichTextLabel;
+      _enterText.Visible = false;
+    }
+
     private void OnDoorEntered(object body)
     {
       if (body is KinematicBody2D kinematicBody2D)
-      {
-        GD.Print("entered door!");
-      }
+        _enterText.Visible = true;
+    }
+
+    private void OnDoorBodyExited(object body)
+    {
+      if (body is KinematicBody2D kinematicBody2D)
+        _enterText.Visible = false;
     }
   }
 }
