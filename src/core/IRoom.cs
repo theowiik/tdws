5abe6ae0e1593c6a59fe8d73369e324bccfa1e6c
Theo Wiik <theo.le.wiik@gmail.com>
2020-01-31 15:48:28 +0100

    add IRoom

---
+++ src/core/IRoom.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Godot;
+using tdws.actors.monsters.abstract_monster;
+using tdws.objects.door;
+
+namespace tdws.core
+{
+  public interface IRoom
+  {
+    IList<Door> GetDoors();
+
+    IList<AbstractMonster> GetEnemies();
+
+    Vector2 GetSpawnPoint();
+  }
+}
