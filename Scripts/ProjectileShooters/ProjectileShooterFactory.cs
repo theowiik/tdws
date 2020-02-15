2d70819fbda8a2d253b37f308be2b63a5b7bf2ae
theowiik <theo.le.wiik@gmail.com>
2020-02-15 11:48:25 +0100

    clean up projectile shooter factory

--- Scripts/ProjectileShooters/ProjectileShooterFactory.cs
+++ Scripts/ProjectileShooters/ProjectileShooterFactory.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using Godot;
+using tdws.Scripts.Services;
 
 namespace tdws.Scripts.ProjectileShooters
 {
@@ -8,67 +9,24 @@ namespace tdws.Scripts.ProjectileShooters
   /// </summary>
   public static class ProjectileShooterFactory
   {
-    /// <summary>
-    ///   Creates and returns a basic automatic rifle.
-    /// </summary>
-    /// <returns>
-    ///   Creates and returns a basic automatic rifle.
-    /// </returns>
-    /// <exception cref="FileNotFoundException">
-    ///   If the assault rifle scene is not found.
-    /// </exception>
     public static IProjectileShooter CreateAssaultRifle()
     {
-      if (!(GD.Load("res://src/projectile_shooters/assault_rifle/AssaultRifle.tscn") is PackedScene
-        projectileShooter))
-        throw new FileNotFoundException("Could not find AssaultRifle.tscn");
-
-      return projectileShooter.Instance() as IProjectileShooter;
+      return NodeService.InstanceNotNull<AbstractProjectileShooter>("res://Scenes/ProjectileShoters/AssaultRifle.tscn");
     }
 
-    /// <summary>
-    ///   Creates and returns a basic shotgun.
-    /// </summary>
-    /// <returns>
-    ///   Creates and returns a basic shotgun.
-    /// </returns>
-    /// <exception cref="FileNotFoundException">
-    ///   If the shotgun scene is not found.
-    /// </exception>
     public static IProjectileShooter CreateShotgun()
     {
-      if (!(GD.Load("res://src/projectile_shooters/shotgun/Shotgun.tscn") is PackedScene
-        projectileShooter))
-        throw new FileNotFoundException("Could not find Shotgun.tscn");
-
-      return projectileShooter.Instance() as IProjectileShooter;
+      return NodeService.InstanceNotNull<AbstractProjectileShooter>("res://Scenes/ProjectileShoters/Shotgun.tscn");
     }
 
-    /// <summary>
-    ///   Creates and returns a wonky gun.
-    /// </summary>
-    /// <returns>
-    ///   A wonky gun.
-    /// </returns>
-    /// <exception cref="FileNotFoundException">
-    ///   If the projectile shooter scene is not found.
-    /// </exception>
     public static IProjectileShooter CreateWonkyGun()
     {
-      if (!(GD.Load("res://src/projectile_shooters/wonky_gun/WonkyGun.tscn") is PackedScene
-        projectileShooter))
-        throw new FileNotFoundException("Could not find WonkyGun.tscn");
-
-      return projectileShooter.Instance() as IProjectileShooter;
+      return NodeService.InstanceNotNull<AbstractProjectileShooter>("res://Scenes/ProjectileShoters/WonkyGun.tscn");
     }
 
     public static IProjectileShooter CreateAlienGun()
     {
-      if (!(GD.Load("res://src/projectile_shooters/alien_gun/AlienGun.tscn") is PackedScene
-        projectileShooter))
-        throw new FileNotFoundException("Could not find AlienGun.tscn");
-
-      return projectileShooter.Instance() as IProjectileShooter;
+      return NodeService.InstanceNotNull<AbstractProjectileShooter>("res://Scenes/ProjectileShoters/AlienGun.tscn");
     }
   }
 }
