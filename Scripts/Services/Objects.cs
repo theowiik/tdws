77b6ccd5542a0fb57acfe04d90ae1feca24719d1
Theo Wiik <theo.le.wiik@gmail.com>
2020-02-17 21:49:23 +0100

    add doc to RequireNonNull

--- Scripts/Services/Objects.cs
+++ Scripts/Services/Objects.cs
@@ -7,6 +7,15 @@ namespace tdws.Scripts.Services
   /// </summary>
   public static class Objects
   {
+    /// <summary>
+    ///   Checks if given object is null, throws null pointer exception if it is null.
+    ///   Returns the object otherwise.
+    /// </summary>
+    /// <param name="obj">The object to check if null.</param>
+    /// <param name="message">Message to print if it null.</param>
+    /// <typeparam name="T">The type of obj.</typeparam>
+    /// <exception cref="NullReferenceException">If the object is null.</exception>
+    /// <returns>The object.</returns>
     public static T RequireNonNull<T>(T obj, string message = "some message")
     {
       if (obj == null)
